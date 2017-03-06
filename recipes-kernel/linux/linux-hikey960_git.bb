96boards/meta-96boards

commit 9b34cb74ebed3d5068b58642f929919748e776c9

linux-hikey960: whitelist DATETIME in do_deploy variables
Signed-off-by: Koen Kooi <koen.kooi@linaro.org>

recipes-kernel/linux/linux-hikey960_git.bb
@@ -58,6 +58,10 @@ do_configure() {
     yes '' | oe_runmake -C ${S} O=${B} oldconfig
 }
 
+# Exclude DATETIME for signatures to avoid invalidating them during a build
+BOOT_IMAGE_BASE_NAME[vardepsexclude] = "DATETIME"
+DT_IMAGE_BASE_NAME [vardepsexclude] = "DATETIME"
+
 BOOT_IMAGE_BASE_NAME = "boot-${PKGV}-${PKGR}-${MACHINE}-${DATETIME}"
 DT_IMAGE_BASE_NAME = "dt-${PKGV}-${PKGR}-${MACHINE}-${DATETIME}"
 

commit 3eabe3a8f0f970ed8b542404aa7ae770fd4153a4

linux-hikey960: fix the whitespace error in the recipe
commit 9b34cb74ebed3d5068b58642f929919748e776c9 introduced
an extra space character which broke the builds.

Signed-off-by: Andrey Konovalov <andrey.konovalov@linaro.org>

recipes-kernel/linux/linux-hikey960_git.bb
@@ -60,7 +60,7 @@ do_configure() {
 
 # Exclude DATETIME for signatures to avoid invalidating them during a build
 BOOT_IMAGE_BASE_NAME[vardepsexclude] = "DATETIME"
-DT_IMAGE_BASE_NAME [vardepsexclude] = "DATETIME"
+DT_IMAGE_BASE_NAME[vardepsexclude] = "DATETIME"
 
 BOOT_IMAGE_BASE_NAME = "boot-${PKGV}-${PKGR}-${MACHINE}-${DATETIME}"
 DT_IMAGE_BASE_NAME = "dt-${PKGV}-${PKGR}-${MACHINE}-${DATETIME}"