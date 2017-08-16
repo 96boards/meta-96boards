96boards/meta-96boards

commit 937537569383b564dbdf9d2269550cd12daa8d38

linux-generic: sync LKFT kernel recipes
* make AOSP kernel recipes generic
* add intel-core2-32 machine on AOSP
* ensure STUB_CLK_HI6220 is enabled on Aarch64

Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

recipes-kernel/linux/linux-generic-aosp_4.4.bb
@@ -1,19 +1,18 @@
 require linux.inc
 require kselftests.inc
 
-DESCRIPTION = "Generic Linux Stable 4.4"
+DESCRIPTION = "AOSP kernel for HiKey"
 
 PV = "4.4+git${SRCPV}"
-SRCREV_kernel = "5fc13741fc3a013f67ed3c4dd70c9ed2e9ded971"
+SRCREV_kernel = "f982d16336a48c51d0ef158dd2634d432cc5dd43"
 SRCREV_FORMAT = "kernel"
 
 SRC_URI = "\
-    git://git.linaro.org/people/sumit.semwal/linux-lts.git;protocol=https;nobranch=1;name=kernel \
+    git://android.googlesource.com/kernel/hikey-linaro;protocol=https;branch=android-hikey-linaro-4.4;name=kernel \
     file://distro-overrides.config;subdir=git/kernel/configs \
     file://systemd.config;subdir=git/kernel/configs \
     file://0001-selftests-lib-add-config-fragment-for-bitmap-printf-.patch \
     file://0005-selftests-create-cpufreq-kconfig-fragments.patch \
-    file://0001-selftests-sync-add-config-fragment-for-testing-sync-.patch \
     file://0002-4.4-selftests-ftrace-add-config-fragment.patch \
     file://0003-4.4-selftests-vm-add-config-fragment-fragment.patch \
     file://0004-4.4-selftests-firmware-add-config-fragment-fragment.patch \
@@ -87,5 +86,3 @@ do_deploy_append() {
     cp -a ${B}/defconfig ${DEPLOYDIR}
     cp -a ${B}/.config ${DEPLOYDIR}/config
 }
-
-require machine-specific-hooks.inc

recipes-kernel/linux/linux-generic-aosp_4.9.bb
@@ -1,25 +1,22 @@
 require linux.inc
 require kselftests.inc
 
-DESCRIPTION = "Generic Linux Stable 4.4"
+DESCRIPTION = "AOSP kernel for HiKey"
 
-PV = "4.4+git${SRCPV}"
-SRCREV_kernel = "5fc13741fc3a013f67ed3c4dd70c9ed2e9ded971"
+PV = "4.9+git${SRCPV}"
+SRCREV_kernel = "465c718d6948d54c9ec2b477668c45766f7a3236"
 SRCREV_FORMAT = "kernel"
 
 SRC_URI = "\
-    git://git.linaro.org/people/sumit.semwal/linux-lts.git;protocol=https;nobranch=1;name=kernel \
+    git://android.googlesource.com/kernel/hikey-linaro;protocol=https;branch=android-hikey-linaro-4.9;name=kernel \
     file://distro-overrides.config;subdir=git/kernel/configs \
     file://systemd.config;subdir=git/kernel/configs \
     file://0001-selftests-lib-add-config-fragment-for-bitmap-printf-.patch \
+    file://0002-selftests-ftrace-add-CONFIG_KPROBES-y-to-the-config-.patch \
+    file://0003-selftests-vm-add-CONFIG_SYSVIPC-y-to-the-config-frag.patch \
+    file://0004-selftests-gpio-add-config-fragment-for-gpio-mockup.patch \
     file://0005-selftests-create-cpufreq-kconfig-fragments.patch \
-    file://0001-selftests-sync-add-config-fragment-for-testing-sync-.patch \
-    file://0002-4.4-selftests-ftrace-add-config-fragment.patch \
-    file://0003-4.4-selftests-vm-add-config-fragment-fragment.patch \
-    file://0004-4.4-selftests-firmware-add-config-fragment-fragment.patch \
-    file://0005-4.4-selftests-static_keys-add-config-fragment-fragment.patch \
-    file://0006-4.4-selftests-user-add-config-fragment-fragment.patch \
-    file://0007-4.4-selftests-zram-add-config-fragment-fragment.patch \
+    file://0001-selftests-sync-add-config-fragment-for-testing-sync-.patch;apply=no \
 "
 
 S = "${WORKDIR}/git"
@@ -48,6 +45,9 @@ do_configure() {
       ;;
     esac
 
+    # Make sure to enable NUMA
+    echo 'CONFIG_NUMA=y' >> ${B}/.config
+
     # Check for kernel config fragments. The assumption is that the config
     # fragment will be specified with the absolute path. For example:
     #   * ${WORKDIR}/config1.cfg
@@ -73,11 +73,9 @@ do_configure() {
         ( cd ${WORKDIR} && ${S}/scripts/kconfig/merge_config.sh -m -r -O ${B} ${B}/.config ${KERNEL_CONFIG_FRAGMENTS} 1>&2 )
     fi
 
-    # Since kselftest-merge target isn't available, merge the individual
-    # selftests config fragments included in the kernel source tree
-    ( cd ${WORKDIR} && ${S}/scripts/kconfig/merge_config.sh -m -r -O ${B} ${B}/.config ${S}/tools/testing/selftests/*/config 1>&2 )
-
     oe_runmake -C ${S} O=${B} olddefconfig
+
+    oe_runmake -C ${S} O=${B} kselftest-merge
 
     bbplain "Saving defconfig to:\n${B}/defconfig"
     oe_runmake -C ${B} savedefconfig
@@ -87,5 +85,3 @@ do_deploy_append() {
     cp -a ${B}/defconfig ${DEPLOYDIR}
     cp -a ${B}/.config ${DEPLOYDIR}/config
 }
-
-require machine-specific-hooks.inc

recipes-kernel/linux/linux-generic-lts-rc_4.4.bb
@@ -41,6 +41,7 @@ do_configure() {
     case "${HOST_ARCH}" in
       aarch64)
         cp ${S}/arch/arm64/configs/defconfig ${B}/.config
+        echo 'CONFIG_STUB_CLK_HI6220=y' >> ${B}/.config
       ;;
       x86_64)
         cp ${S}/arch/x86/configs/x86_64_defconfig ${B}/.config

recipes-kernel/linux/linux-hikey-aosp_4.4.bb
@@ -1,78 +1,1 @@
-require linux.inc
-require kselftests.inc
-
-DESCRIPTION = "AOSP kernel for HiKey"
-
-PV = "4.4+git${SRCPV}"
-SRCREV_kernel = "f982d16336a48c51d0ef158dd2634d432cc5dd43"
-SRCREV_FORMAT = "kernel"
-
-SRC_URI = "\
-    git://android.googlesource.com/kernel/hikey-linaro;protocol=https;branch=android-hikey-linaro-4.4;name=kernel \
-    file://distro-overrides.config;subdir=git/kernel/configs \
-    file://systemd.config;subdir=git/kernel/configs \
-    file://0001-selftests-lib-add-config-fragment-for-bitmap-printf-.patch \
-    file://0005-selftests-create-cpufreq-kconfig-fragments.patch \
-    file://0002-4.4-selftests-ftrace-add-config-fragment.patch \
-    file://0003-4.4-selftests-vm-add-config-fragment-fragment.patch \
-    file://0004-4.4-selftests-firmware-add-config-fragment-fragment.patch \
-    file://0005-4.4-selftests-static_keys-add-config-fragment-fragment.patch \
-    file://0006-4.4-selftests-user-add-config-fragment-fragment.patch \
-    file://0007-4.4-selftests-zram-add-config-fragment-fragment.patch \
-"
-
-S = "${WORKDIR}/git"
-
-COMPATIBLE_MACHINE = "hikey"
-KERNEL_IMAGETYPE ?= "Image"
-KERNEL_CONFIG_FRAGMENTS += "\
-    ${S}/kernel/configs/distro-overrides.config \
-    ${S}/kernel/configs/systemd.config \
-"
-
-# make[3]: *** [scripts/extract-cert] Error 1
-DEPENDS += "openssl-native"
-HOST_EXTRACFLAGS += "-I${STAGING_INCDIR_NATIVE}"
-
-do_configure() {
-    cp ${S}/arch/arm64/configs/hikey_defconfig ${B}/.config
-
-    # Check for kernel config fragments. The assumption is that the config
-    # fragment will be specified with the absolute path. For example:
-    #   * ${WORKDIR}/config1.cfg
-    #   * ${S}/config2.cfg
-    # Iterate through the list of configs and make sure that you can find
-    # each one. If not then error out.
-    # NOTE: If you want to override a configuration that is kept in the kernel
-    #       with one from the OE meta data then you should make sure that the
-    #       OE meta data version (i.e. ${WORKDIR}/config1.cfg) is listed
-    #       after the in-kernel configuration fragment.
-    # Check if any config fragments are specified.
-    if [ ! -z "${KERNEL_CONFIG_FRAGMENTS}" ]; then
-        for f in ${KERNEL_CONFIG_FRAGMENTS}; do
-            # Check if the config fragment was copied into the WORKDIR from
-            # the OE meta data
-            if [ ! -e "$f" ]; then
-                echo "Could not find kernel config fragment $f"
-                exit 1
-            fi
-        done
-
-        # Now that all the fragments are located merge them.
-        ( cd ${WORKDIR} && ${S}/scripts/kconfig/merge_config.sh -m -r -O ${B} ${B}/.config ${KERNEL_CONFIG_FRAGMENTS} 1>&2 )
-    fi
-
-    # Since kselftest-merge target isn't available, merge the individual
-    # selftests config fragments included in the kernel source tree
-    ( cd ${WORKDIR} && ${S}/scripts/kconfig/merge_config.sh -m -r -O ${B} ${B}/.config ${S}/tools/testing/selftests/*/config 1>&2 )
-
-    oe_runmake -C ${S} O=${B} olddefconfig
-
-    bbplain "Saving defconfig to:\n${B}/defconfig"
-    oe_runmake -C ${B} savedefconfig
-}
-
-do_deploy_append() {
-    cp -a ${B}/defconfig ${DEPLOYDIR}
-    cp -a ${B}/.config ${DEPLOYDIR}/config
-}
+linux-generic-aosp_4.4.bb

recipes-kernel/linux/linux-hikey-aosp_4.9.bb
@@ -1,77 +1,1 @@
-require linux.inc
-require kselftests.inc
-
-DESCRIPTION = "AOSP kernel for HiKey"
-
-PV = "4.9+git${SRCPV}"
-SRCREV_kernel = "465c718d6948d54c9ec2b477668c45766f7a3236"
-SRCREV_FORMAT = "kernel"
-
-SRC_URI = "\
-    git://android.googlesource.com/kernel/hikey-linaro;protocol=https;branch=android-hikey-linaro-4.9;name=kernel \
-    file://distro-overrides.config;subdir=git/kernel/configs \
-    file://systemd.config;subdir=git/kernel/configs \
-    file://0001-selftests-lib-add-config-fragment-for-bitmap-printf-.patch \
-    file://0002-selftests-ftrace-add-CONFIG_KPROBES-y-to-the-config-.patch \
-    file://0003-selftests-vm-add-CONFIG_SYSVIPC-y-to-the-config-frag.patch \
-    file://0004-selftests-gpio-add-config-fragment-for-gpio-mockup.patch \
-    file://0005-selftests-create-cpufreq-kconfig-fragments.patch \
-    file://0001-selftests-sync-add-config-fragment-for-testing-sync-.patch;apply=no \
-"
-
-S = "${WORKDIR}/git"
-
-COMPATIBLE_MACHINE = "hikey"
-KERNEL_IMAGETYPE ?= "Image"
-KERNEL_CONFIG_FRAGMENTS += "\
-    ${S}/kernel/configs/distro-overrides.config \
-    ${S}/kernel/configs/systemd.config \
-"
-
-# make[3]: *** [scripts/extract-cert] Error 1
-DEPENDS += "openssl-native"
-HOST_EXTRACFLAGS += "-I${STAGING_INCDIR_NATIVE}"
-
-do_configure() {
-    cp ${S}/arch/arm64/configs/hikey_defconfig ${B}/.config
-
-    # Make sure to enable NUMA
-    echo 'CONFIG_NUMA=y' >> ${B}/.config
-
-    # Check for kernel config fragments. The assumption is that the config
-    # fragment will be specified with the absolute path. For example:
-    #   * ${WORKDIR}/config1.cfg
-    #   * ${S}/config2.cfg
-    # Iterate through the list of configs and make sure that you can find
-    # each one. If not then error out.
-    # NOTE: If you want to override a configuration that is kept in the kernel
-    #       with one from the OE meta data then you should make sure that the
-    #       OE meta data version (i.e. ${WORKDIR}/config1.cfg) is listed
-    #       after the in-kernel configuration fragment.
-    # Check if any config fragments are specified.
-    if [ ! -z "${KERNEL_CONFIG_FRAGMENTS}" ]; then
-        for f in ${KERNEL_CONFIG_FRAGMENTS}; do
-            # Check if the config fragment was copied into the WORKDIR from
-            # the OE meta data
-            if [ ! -e "$f" ]; then
-                echo "Could not find kernel config fragment $f"
-                exit 1
-            fi
-        done
-
-        # Now that all the fragments are located merge them.
-        ( cd ${WORKDIR} && ${S}/scripts/kconfig/merge_config.sh -m -r -O ${B} ${B}/.config ${KERNEL_CONFIG_FRAGMENTS} 1>&2 )
-    fi
-
-    oe_runmake -C ${S} O=${B} olddefconfig
-
-    oe_runmake -C ${S} O=${B} kselftest-merge
-
-    bbplain "Saving defconfig to:\n${B}/defconfig"
-    oe_runmake -C ${B} savedefconfig
-}
-
-do_deploy_append() {
-    cp -a ${B}/defconfig ${DEPLOYDIR}
-    cp -a ${B}/.config ${DEPLOYDIR}/config
-}
+linux-generic-aosp_4.9.bb

commit e9fc1d748bf2367cb5efd8fbd9c0b4cf53a61402

linux-generic-aosp: use hikey_defconfig
Fix regression introduced by commit
937537569383b564dbdf9d2269550cd12daa8d38

Wrong copy/paste has renamed the source defconfig.
arm64 defconfig is used instead of hikey_defconfig.
Revert to use hikey_defconfig.

Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

recipes-kernel/linux/linux-generic-aosp_4.4.bb
@@ -39,8 +39,7 @@ do_configure() {
     # the kernel config file has a different name.
     case "${HOST_ARCH}" in
       aarch64)
-        cp ${S}/arch/arm64/configs/defconfig ${B}/.config
-        echo 'CONFIG_STUB_CLK_HI6220=y' >> ${B}/.config
+        cp ${S}/arch/arm64/configs/hikey_defconfig ${B}/.config
       ;;
       arm)
         cp ${S}/arch/arm/configs/multi_v7_defconfig ${B}/.config

recipes-kernel/linux/linux-generic-aosp_4.9.bb
@@ -37,8 +37,7 @@ do_configure() {
     # the kernel config file has a different name.
     case "${HOST_ARCH}" in
       aarch64)
-        cp ${S}/arch/arm64/configs/defconfig ${B}/.config
-        echo 'CONFIG_STUB_CLK_HI6220=y' >> ${B}/.config
+        cp ${S}/arch/arm64/configs/hikey_defconfig ${B}/.config
       ;;
       arm)
         cp ${S}/arch/arm/configs/multi_v7_defconfig ${B}/.config